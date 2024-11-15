cc092057f9e18b90728759511a89b3d89ce167cb
Andrey <andrey.mikhaylov.n@gmail.com>
2024-11-15 09:52:30 +0300

    done

--- tests_12_1.py
+++ tests_12_1.py
@@ -1,9 +1,50 @@
-def test():
-    pass
+import unittest
+from HumanMoveTest.runner import Runner
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        """
+        test_walk - метод, в котором создаётся объект класса Runner с произвольным именем.
+        Далее вызовите метод walk у этого объекта 10 раз.
+        После чего методом assertEqual сравните distance этого объекта со значением 50.
+        """
+        runner = Runner('name')
+        for _ in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+
+    def test_run(self):
+        """
+        test_run - метод, в котором создаётся объект класса Runner с произвольным именем.
+        Далее вызовите метод run у этого объекта 10 раз.
+        После чего методом assertEqual сравните distance этого объекта со значением 100.
+        """
+        runner = Runner('name')
+        for _ in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        """
+        test_challenge - метод в котором создаются 2 объекта класса Runner с произвольными именами.
+        Далее 10 раз у объектов вызываются методы run и walk соответственно.
+        Т.к. дистанции должны быть разными, используйте метод assertNotEqual, чтобы убедится в неравенстве результатов.
+        """
+        runner1 = Runner('name1')
+        runner2 = Runner('name1')
+        for _ in range(10):
+            runner1.walk()
+            runner2.run()
+        self.assertNotEqual(runner1.distance, runner2.distance)
 
 
 if __name__ == '__main__':
-    test()
+    unittest.main()
+    """
+    Вывод на консоль:
+    Ran 3 tests in 0.001s OK
+    """
 
 
 """
